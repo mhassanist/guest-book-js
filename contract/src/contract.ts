4dcc9c0d882329a69d7228456894f87f91870a2c
Mohammed Saudi <msaudi.hassan@st.uskudar.edu.tr>
2022-09-09 10:46:47 +0300

    return last 20 message reversed

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -1,24 +1,28 @@
-import { NearBindgen, near, call, view } from 'near-sdk-js'
-import { POINT_ONE, PostedMessage } from './model'
+import { NearBindgen, near, call, view } from "near-sdk-js"
+import { POINT_ONE, PostedMessage } from "./model"
 
 @NearBindgen({})
 class GuestBook {
-  messages: PostedMessage[] = [];
+  messages: PostedMessage[] = []
 
   @call
   // Public - Adds a new message.
   add_message({ text }: { text: string }) {
     // If the user attaches more than 0.01N the message is premium
-    const premium = near.attachedDeposit() >= BigInt(POINT_ONE);
-    const sender = near.predecessorAccountId();
+    const premium = near.attachedDeposit() >= BigInt(POINT_ONE)
+    const sender = near.predecessorAccountId()
 
-    const message = new PostedMessage({premium, sender, text});
-    this.messages.push(message);
+    const message = new PostedMessage({ premium, sender, text })
+    this.messages.push(message)
   }
-  
+
   @view
   // Returns an array of messages.
-  get_messages({ from_index = 0, limit = 10 }: { from_index: number, limit: number }): PostedMessage[] {
-    return this.messages.slice(from_index, from_index + limit);
+  get_messages(): PostedMessage[] {
+    if (this.messages.length <= 20) {
+      return this.messages.reverse()
+    } else {
+      return this.messages.slice(-20).reverse()
+    }
   }
 }
